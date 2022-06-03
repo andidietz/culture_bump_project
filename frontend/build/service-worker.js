bf3af9ef3e10cc7a42e72c3a15ae82003f511a80
andidietz <alexandradietz3@gmail.com>
2022-06-03 16:09:16 -0500

    update files

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fece3ab289402ebfc39aad34c68fb628.js"
+  "/precache-manifest.762fa1b4f2ddcf71a4607e7384959eee.js"
 );
 
 workbox.clientsClaim();
